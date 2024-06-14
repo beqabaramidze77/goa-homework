7ae7bea05d75d1db3a45a7e2be793a55861bd4fb
beqabaramidze77 <beqabaramidze22@gmail.com>
2024-06-14 19:13:46 +0400

    goa homework

---
+++ level 02/level2.py
@@ -0,0 +1,10 @@
+Akaki Sixarulidze
+Aleksandre Ghomidze
+Aleqsandre Chivadze
+Alexandre Katsarava
+Ana Tetrashvili
+Andria Svanidze
+Atuka Khuskivadze
+Barbare Tinikashvili
+Bayayi Bayayovichi Bayayashvili
+Beridze Mate
